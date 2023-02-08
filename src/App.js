83222d01d609f469a5c84881b716622a3b0ca4ec
Ethan Buchanan <Ethan.leslieb@gmail.com>
2023-02-08 11:24:02 -0700

    completed mvp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {useState, useEffect} from "react"
+
+import axios from "axios"
+
+import Character from "./components/Character"
 
 const App = () => {
+  const [characters, setCharacters] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -8,9 +14,23 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((res) => {
+        setCharacters(res.data);
+      })
+      .catch((err) => console.log("GET FAILED", err));
+  }, []);
+
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map((person) => (
+        <Character key={person.name} data={person}/>
+      ))}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+
+
+ function Character(props) {
+    return (
+        <div className= 'container'>
+            <h1> {props.data.name} </h1>
+        </div>
+    )
+}
+
+
+
+export default Character;
